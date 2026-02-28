9ea39ba68c4032bb730f736bb42d1b19b6215cd9
ValentinaRozo <valerozobernal@gmail.com>
2026-02-27 22:00:36 -0500

    Balon

--- Assets/Scripts/PlayerPass.cs
+++ Assets/Scripts/PlayerPass.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerPass : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public BallPhysics ball;
+    public Transform teammate;
 
-    // Update is called once per frame
+    public float passRange = 2f;
+    public float passForce = 6f;
+
     void Update()
     {
-        
+        // Usa Space si quieres
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TryPassToTeammate();
+        }
+    }
+
+    void TryPassToTeammate()
+    {
+        if (ball == null || teammate == null) return;
+
+        // Solo si la pelota está cerca del jugador
+        if (Vector3.Distance(transform.position, ball.transform.position) > passRange)
+            return;
+
+        Vector3 dir = (teammate.position - ball.transform.position);
+        if (dir.sqrMagnitude < 0.01f) return;
+
+        ball.Impulse(dir, passForce);
     }
 }
